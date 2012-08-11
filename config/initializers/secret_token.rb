78c9593a45ecf84bcac3013141f7256f81fe8b1c
Patrick O'Dwyer <kertap_raid@yahoo.co.uk>
2012-08-11 13:28:03 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Birthday::Application.config.secret_token = '1e0a18f7ef7058a62a6f60bf494ccafd863eee8e6231848a9c15172b64f0ee96ce3341d2647527314de8481b9d3f520a660ddc2e70ca7d6b105b73170ba19deb'
